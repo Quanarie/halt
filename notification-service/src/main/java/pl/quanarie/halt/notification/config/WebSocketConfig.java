2e919c96f0742dcde2f9f8eaafa9032dcb22b478
Quanarie <qustaine@gmail.com>
2026-03-12 14:13:23 +0100

    notification-service

---
+++ notification-service/src/main/java/pl/quanarie/halt/notification/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package pl.quanarie.halt.notification.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    public static String CLIENT_COMMUNICATION_TOPIC = "/client";
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker(CLIENT_COMMUNICATION_TOPIC);
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/notifications")
+                .setAllowedOrigins("*"); // TODO: limit in prod
+    }
+}
